4836dc335af568dc3abdb0d169c1fa7bf79cee71
Marcos Cannabrava <mpcannabrava@gmail.com>
2020-05-01 10:13:49 -0300

    remove trash

--- trash/app/connectDb.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-export default (): Promise<void> => new Promise((resolve, reject) => {
-  mongoose.Promise = global.Promise
-  mongoose.connect(process.env.DB)
-  mongoose.connection.on('open', resolve)
-  mongoose.connection.on('error', reject)
-})
--- trash/db/helpers.ts
+++
@@ -1,4 +0,0 @@
-import {Id} from './types'
-
-export const isObjectId = (id: Id): boolean =>
-  /^[0-9a-fA-F]{24}$/.test(id)
